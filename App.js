0b9d5f637ace5ac0292bfe1c9b0c581afe684750
Elijah Temwani Zulu <modeaxe@Gmail.com>
2021-02-25 23:33:57 -0500

    home page button initial setup

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
+
+  const [shake, setShake] = useState('none')
+
+  const shakee = () => {
+    setShake('True');
+  }
+  const shaker = () => {
+    setShake('False');
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{shake}</Text>
+      <View style={styles.shaker}>
+        <Button title='Shaker' onPress={shaker}/>
+      </View>
+
+      <View style={styles.shakee}>
+      <Button title='Shakee' onPress={shakee}/>
+      </View>      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +36,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  shaker:{
+    backgroundColor: '#87ceeb',
+    padding: 20,
+  },
+  shakee:{
+    backgroundColor: '#ffb4b4',
+    padding: 20,
+  }  
 });
+
